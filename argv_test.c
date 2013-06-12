d9bffba4cdc4b9068bf29e9a2cd3aede751d638e
Leibovich, Elazar <elazarl@gmail.com>
2013-06-12 14:21:17 +0300

    whitespace

--- argv_test.c
+++ argv_test.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <setjmp.h>
 
 #include "argv.h"
 #include "tap.h"
 
 void argv_test() {
-	argv_t args[] = { {"--test", "file1"}, {"--nice", "1"}, {"--null", NULL} , ARGV_T_END};
-	ok1(strcmp(argv_get(args, "--test"), "file1") == 0);
-	ok1(argv_get(args, "--test1") == NULL);
+    argv_t args[] = { {"--test", "file1"}, {"--nice", "1"}, {"--null", NULL} , ARGV_T_END};
+    ok1(strcmp(argv_get(args, "--test"), "file1") == 0);
+    ok1(argv_get(args, "--test1") == NULL);
 
-	if (1) {
-		/* TODO: find a way to test panics */
-		char *argv[] = {"./a.out", "--test", "a", "--null", "hopa"};
-		ok(argv_parse(5, argv, args), "all required arguments set");
-		ok1(strcmp(argv_get(args, "--test"), "a") == 0);
-	}
+    if (1) {
+    	/* TODO: find a way to test panics */
+    	char *argv[] = {"./a.out", "--test", "a", "--null", "hopa"};
+    	ok(argv_parse(5, argv, args), "all required arguments set");
+    	ok1(strcmp(argv_get(args, "--test"), "a") == 0);
+    }
 }
