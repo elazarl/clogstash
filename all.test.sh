8a03cc495dee7f164589368d33d4f36185211583
Elazar Leibovich <elazarl@gmail.com>
2013-06-18 23:28:51 +0300

    SLOPPY=y to disable Werror, KEEP=y to keep test exec

--- all.test.sh
+++ all.test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #C_FILES=$(find . -maxdepth 1 -name \*.c|sed '/_test\.c/d;/tap\./d;/logcat\.c/d'| tr '\n' ' ')
-C_FLAGS="$C_FLAGS -ggdb3 -Wall -Wextra"
+WERROR="-Werror"
+[ "$SLOPPY" = "y" ] && WERROR=""
+C_FLAGS="$C_FLAGS -ggdb3 -Wall -Wextra $WERROR"
 [ -f _main_test.c ] && rm _main_test.c
 TEST_FILES=$(find . -maxdepth 1 -name \*_test.c)
 for testfile in $TEST_FILES; do
@@ -39,5 +41,5 @@ if [ "$BASENAME" != "all.test.sh" ]; then
 	RUNWITH=${RUNWITH//__/ }
 fi
 $RUNWITH ./_test_main.out $@ || (echo TEST FAILED;exit -1) && echo SUCCESS
-#rm _test_main.out
+[ -z "$KEEP" ] && rm _test_main.out
 rm _main_test.c
