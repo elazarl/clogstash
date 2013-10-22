766d814aa396b84b8365e3e12f17378ea45dfc74
Leibovich, Elazar <elazarl@gmail.com>
2013-10-22 07:45:32 +0200

    use signed char for byte buffers

--- buf.h
+++ buf.h
@@ -1,16 +1,16 @@
 #ifndef __H_BUF_
 #define __H_BUF_
 struct buf {
-    unsigned char *buf;
+    char *buf;
     int len;
 };
 
 struct buf buf_slice_from(struct buf b, int from);
 struct buf buf_slice_to(struct buf b, int to);
 struct buf buf_copy(struct buf b);
-struct buf buf_wrap(unsigned char *b, int len);
+struct buf buf_wrap(char *b, int len);
 int buf_empty(struct buf b);
-
+struct buf buf_from_str(char *dat);
 
 /**
  * reads from fd to buffer, returns number of bytes read or -1 on error.
