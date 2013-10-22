6a14c5f9daf339a97b02e4bf45c7c418859f4c7e
Leibovich, Elazar <elazarl@gmail.com>
2013-10-22 11:13:15 +0200

    bug: target specific file (foo_darwin.c) always fetched

--- files.py
+++ files.py
@@ -13,12 +13,16 @@ if len(sys.argv) > 1 and sys.argv[1] == 'tests':
     addtests = True
 
 cfiles = []
+def relevant(f):
+    for t in irrelevant_targets:
+        if f.endswith('_' + t + '.c'):
+            return False
+    return True
 for f in os.listdir('.'):
     if not f.endswith('.c'):
         continue
-    for t in targets:
-        if t.endswith('_' + t + '.c'):
-            continue
+    if not relevant(f):
+        continue
     if f == 'main.c' and addtests:
         continue
     if f == 'tap.c' and not addtests:
